c8f610eb9b14d78ef21589cecde720913957e9fe
876-samurai <c.mcintosh876@gmail.com>
2023-03-08 20:30:44 -0500

    Added expense date component and basic styling

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,17 +1,11 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItems.css";
 
 function ExpenseItems(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day =props.date.toLocaleString("en-US", {day:'2-digit'})
-  const year = props.date.getFullYear()
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div>
         <h2 className="expense-item__description">{props.title}</h2>
       </div>
